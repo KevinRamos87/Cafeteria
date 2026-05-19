70457ecafef2c8bc4dfd1e999add49422e3f97d9
danielRamos <daniel.r.c.dk23@gmail.com>
2026-05-19 15:25:34 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp3/Bebida.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Bebida
+    {
+        private string nombre;
+        private string tamano;
+        private float precio;
+
+        public Bebida(string nombre, string tamano, float precio)
+        {
+            this.nombre = nombre;
+            this.tamano = tamano;
+            this.precio = precio;
+        }
+
+        public string preparar()
+        {
+            return ( "Preparando un  " + nombre + " de  tamaño " + tamano); 
+        }
+
+        public void Descuento(float prociento)
+        {
+            float descu = precio * (prociento / 100);
+            precio = precio - descu;
+        }
+
+        public string Descripcion()
+        {
+            return ("Se preparo un:  " + nombre + " de tamaño " + tamano + "De precio $" + precio);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {        
+        static void Main(string[] args)
+        {
+            string nombre;
+            string tamano;
+            float precio;
+
+            Console.WriteLine("Ingrese el nombre de la bebida 1: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el tamaño: ");
+            tamano = Console.ReadLine();
+            Console.WriteLine("Ingrese el costo: ");
+            precio = float.Parse(Console.ReadLine());
+
+            Bebida bebida_n1 = new Bebida(nombre, tamano, precio);
+            Console.Clear();
+
+            Console.WriteLine("Ingrese el nombre de la bebida 2: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el tamaño: ");
+            tamano = Console.ReadLine();
+            Console.WriteLine("Ingrese el costo: ");
+            precio = float.Parse(Console.ReadLine());
+
+            Bebida bebida_n2 = new Bebida(nombre, tamano, precio);
+            Console.Clear();
+
+            Console.WriteLine("Ingrese el nombre de la bebida 3: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese el tamaño: ");
+            tamano = Console.ReadLine();
+            Console.WriteLine("Ingrese el costo: ");
+            precio = float.Parse(Console.ReadLine());
+
+            Bebida bebida_n3 = new Bebida(nombre, tamano, precio);
+            Console.Clear();
+
+            bebida_n1.Descuento(20);
+            bebida_n2.Descuento(10);
+            bebida_n3.Descuento(50);
+
+            Console.WriteLine("Preparando....");
+            Console.WriteLine("\n" + bebida_n1.preparar());
+            Console.WriteLine("\n"+bebida_n2.preparar());
+            Console.WriteLine("\n"+bebida_n2.preparar());
+            
+
+
+            Console.WriteLine("\n Descripcion del pedido....");
+            Console.WriteLine("\n" + bebida_n1.Descripcion());
+            Console.WriteLine("\n" + bebida_n2.Descripcion());
+            Console.WriteLine("\n" + bebida_n2.Descripcion());
+            
+
+        }
+    }
+}
